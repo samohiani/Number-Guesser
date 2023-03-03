9224c091c4faa0a6bd6d8bd1157892c29013e586
dasaka1 <103277834+samohiani@users.noreply.github.com>
2023-03-03 17:34:55 +0000

    Added comment to ease readability and understanding

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-#Number Guessing Game Objectives:
-
-# Include an ASCII art logo.
-# Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player. 
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 import random
 from art import logo
 print(logo)
 
 def difficulty():
+  """Chooses a difficulty for your game"""
   level = input("Choose a diificulty, EASY or HARD: ").lower()
   if level == "easy":
     return 10
@@ -34,6 +26,7 @@ def check_guess(guess, number, lives):
     return lives - 1
       
 def play_game():
+  """Starts game."""
   print("Welcome to Number Guesser!")
   print("I'm thinking of a number between 1 and 100")
   answer = random.choice(range(1,101))
